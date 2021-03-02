30f391a47b683123f8b883636b7634c2f7bc0d37
Peony Lum <peonylum@MacBook-Pro-2.local>
2021-03-02 03:27:36 -0800

    Create Edit for tasks

--- routes/edit.js
+++ routes/edit.js
@@ -1,7 +1,32 @@
 /*
- * GET add tasks page.
+ * GET edit tasks page.
  */
 
-exports.view = function(req, res){
-    res.render('edit');
-  };
+var data = require('../tasks.json');
+
+exports.view = function (req, res) {
+  res.render('edit');
+};
+
+exports.editTask = function (req, res) {
+  let editIndex;
+  data.tasks.forEach((obj, index) => {
+    if (obj.id == req.body.id) {
+      editIndex = index;
+    }
+  })
+
+  if (editIndex !== undefined) {
+    let element = data.tasks[editIndex];
+    let priority = req.body.priority.charAt(0).toUpperCase() + req.body.priority.slice(1);
+
+    element.name = req.body.name;
+    element.dscrp = req.body.dscrp;
+    element.assign = req.body.assign;
+    element.priority = priority;
+    element.calendar = req.body.calendar;
+    element.completed = false;
+  }
+
+  res.redirect('/');
+}
